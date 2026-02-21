7a7a7e5e97d809ab54e3ce0e5aee0635ccdb91ec
Tannhuot KHOUV <khouvtannhuot168@gmail.com>
2026-02-21 16:06:14 +0700

    - route name - create layout

---
+++ resources/views/admin/category/index.blade.php
@@ -0,0 +1,323 @@
+@extends('layout.main')
+
+@section('content')
+    <!-- Page content-->
+    <div class="container my-5">
+      <div class="row">
+        <div class="d-flex justify-content-between mb-2">
+          <h3>Category List</h3>
+          <a class="btn btn-success" href="create_edit.html" role="button"
+            >Create</a
+          >
+        </div>
+        <!-- Blog entries-->
+        <div class="col-lg-12">
+          <div class="card p-3">
+            <table
+              id="datatable"
+              class="table table-striped"
+              style="width: 100%"
+            >
+              <thead>
+                <tr>
+                  <th>No</th>
+                  <th>Category</th>
+                  <th style="width: 100px">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>Page</td>
+                  <td>
+                    <a
+                      class="btn btn-primary btn-sm"
+                      href="create_edit.html"
+                      role="button"
+                      >Edit</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Page</td>
+                  <td>
+                    <a
+                      class="btn btn-primary btn-sm"
+                      href="create_edit.html"
+                      role="button"
+                      >Edit</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Page</td>
+                  <td>
+                    <a
+                      class="btn btn-primary btn-sm"
+                      href="create_edit.html"
+                      role="button"
+                      >Edit</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Page</td>
+                  <td>
+                    <a
+                      class="btn btn-primary btn-sm"
+                      href="create_edit.html"
+                      role="button"
+                      >Edit</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Page</td>
+                  <td>
+                    <a
+                      class="btn btn-primary btn-sm"
+                      href="create_edit.html"
+                      role="button"
+                      >Edit</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Page</td>
+                  <td>
+                    <a
+                      class="btn btn-primary btn-sm"
+                      href="create_edit.html"
+                      role="button"
+                      >Edit</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Page</td>
+                  <td>
+                    <a
+                      class="btn btn-primary btn-sm"
+                      href="create_edit.html"
+                      role="button"
+                      >Edit</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Page</td>
+                  <td>
+                    <a
+                      class="btn btn-primary btn-sm"
+                      href="create_edit.html"
+                      role="button"
+                      >Edit</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Page</td>
+                  <td>
+                    <a
+                      class="btn btn-primary btn-sm"
+                      href="create_edit.html"
+                      role="button"
+                      >Edit</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Page</td>
+                  <td>
+                    <a
+                      class="btn btn-primary btn-sm"
+                      href="create_edit.html"
+                      role="button"
+                      >Edit</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Page</td>
+                  <td>
+                    <a
+                      class="btn btn-primary btn-sm"
+                      href="create_edit.html"
+                      role="button"
+                      >Edit</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Page</td>
+                  <td>
+                    <a
+                      class="btn btn-primary btn-sm"
+                      href="create_edit.html"
+                      role="button"
+                      >Edit</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Page</td>
+                  <td>
+                    <a
+                      class="btn btn-primary btn-sm"
+                      href="create_edit.html"
+                      role="button"
+                      >Edit</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Page</td>
+                  <td>
+                    <a
+                      class="btn btn-primary btn-sm"
+                      href="create_edit.html"
+                      role="button"
+                      >Edit</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Page</td>
+                  <td>
+                    <a
+                      class="btn btn-primary btn-sm"
+                      href="create_edit.html"
+                      role="button"
+                      >Edit</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Page</td>
+                  <td>
+                    <a
+                      class="btn btn-primary btn-sm"
+                      href="create_edit.html"
+                      role="button"
+                      >Edit</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Page</td>
+                  <td>
+                    <a
+                      class="btn btn-primary btn-sm"
+                      href="create_edit.html"
+                      role="button"
+                      >Edit</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Page</td>
+                  <td>
+                    <a
+                      class="btn btn-primary btn-sm"
+                      href="create_edit.html"
+                      role="button"
+                      >Edit</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Page</td>
+                  <td>
+                    <a
+                      class="btn btn-primary btn-sm"
+                      href="create_edit.html"
+                      role="button"
+                      >Edit</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Page</td>
+                  <td>
+                    <a
+                      class="btn btn-primary btn-sm"
+                      href="create_edit.html"
+                      role="button"
+                      >Edit</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Page</td>
+                  <td>
+                    <a
+                      class="btn btn-primary btn-sm"
+                      href="create_edit.html"
+                      role="button"
+                      >Edit</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Page</td>
+                  <td>
+                    <a
+                      class="btn btn-primary btn-sm"
+                      href="create_edit.html"
+                      role="button"
+                      >Edit</a
+                    >
+          `btn-sm"
+                      href="create_edit.html"
+                      role="button"
+                      >Edit</a
+                    >
+                  </td>
+                </tr>
+                <tr>
+                  <td>1</td>
+                  <td>Page</td>
+                  <td>
+                    <a
+                      class="btn btn-primary btn-sm"
+                      href="create_edit.html"
+                      role="button"
+                      >Edit</a
+                    >
+                  </td>
+                </tr>
+              </tbody>
+              <tfoot>
+                <tr>
+                  <th>No</th>
+                  <th>Tag</th>
+                  <th>Action</th>
+                </tr>
+              </tfoot>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
-});
+    $data = [
+        'number'=>5
+    ];
+    return view('index', ['data'=>$data]);
+}) -> name('home');
+
+Route::get('/admin/v2/category', function() {
+    return view('admin.category.index');
+})->name('admin.category');
